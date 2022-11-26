cd3689cad93e510869f6e856f11f48925914dd94
Viktor <viktor.belebezyev@yandex.ru>
2022-11-26 11:21:49 +0300

    Added solution task2

--- HW1/task2.py
+++ HW1/task2.py
@@ -1,1 +1,7 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+for x in range(0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            print(f'Для значения {x}, {y}, {z} истинность выражения является --> '
+                  f'{not(bool(x) or bool(y) or bool(z)) == (not(bool(x)) and not(bool(y)) and not(bool(z)))}')

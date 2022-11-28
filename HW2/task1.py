0c4ed2292549ee872818a7e0ef7a0da5cd88919f
Viktor <viktor.belebezyev@yandex.ru>
2022-11-28 11:54:35 +0300

    added solution task1

--- HW2/task1.py
+++ HW2/task1.py
@@ -2,3 +2,29 @@
 # Пример:
 # 6782 -> 23
 # 0,56 -> 11
+
+print('Программу, принимает на вход вещественное число и показывает сумму его цифр.')
+print()
+
+num = float(input('Введите вещественное число: '))
+
+if num == int(num):
+    sum_num = 0
+    while num > 0:
+        sum_num += (int(num % 10))
+        num //= 10
+    print(f'Сумма цифр в числе равна {sum_num}')
+else:
+    sum_num = 0
+    num = str(num).split('.')
+    num1 = int(num[0])
+    num2 = int(num[1])
+    while num1 > 0:
+        sum_num += (int(num1 % 10))
+        num1 //= 10
+    while num2 > 0:
+        sum_num += (int(num2 % 10))
+        num2 //= 10
+    print(f'Сумма цифр в числе равна {sum_num}')
+
+

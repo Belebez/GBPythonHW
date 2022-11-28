fb6df629c1f4cf829797c4e3511d0443835a0fd7
Viktor <viktor.belebezyev@yandex.ru>
2022-11-28 13:05:16 +0300

    added solution task3

--- HW2/task3.py
+++ HW2/task3.py
@@ -1,1 +1,16 @@
 # Реализуйте алгоритм перемешивания списка. Встроенный алгоритм SHUFFLE не использовать! Реализовать свой метод
+print("Программа создаёт случайный список и перемешивает элементы случайным образом, без использования встроенного метода Shuffle().")
+print()
+
+from random import randint as rnd
+
+size = int(rnd(4, 10))
+list = []
+for i in range(size):
+    list.append(rnd(0, 100))
+print(f'Полученный список - {list}')
+
+for i in range(len(list) - 1, 0, -1):
+    rnd_index = rnd(0, i)
+    list[i], list[rnd_index] = list[rnd_index], list[i]
+print(f'Перемешанный список - {list}')

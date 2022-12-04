ddab64da6f0c353e52a4d2a44f939d234464f8bb
Viktor <viktor.belebezyev@yandex.ru>
2022-12-05 00:51:56 +0300

    added solution task4

--- HW3/task4.py
+++ HW3/task4.py
@@ -3,3 +3,17 @@
 # 45 -> 101101
 # 3 -> 11
 # 2 -> 10
+from random import randint as rnd
+
+print("Программа преобразует десятичное число в двоичное.")
+print()
+
+num_decimal1 = int(rnd(0, 100))
+num_decimal = num_decimal1
+binary = ''
+
+while num_decimal > 0:
+    binary = str(num_decimal % 2) + binary
+    num_decimal //= 2
+
+print(f'Десятичное число {num_decimal1}, в двоичной системе выглядит как - {binary}')
